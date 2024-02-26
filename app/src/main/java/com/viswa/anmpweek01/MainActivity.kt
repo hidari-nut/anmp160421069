aa5453970cd382f0c38b392468b8b82f37d7dd49
hidari-nut <madevis02@gmail.com>
2024-02-26 11:49:22 +0700

    Week 2 Exercise Progress added Name Display and Back Button

--- app/src/main/java/com/viswa/anmpweek01/MainActivity.kt
+++ app/src/main/java/com/viswa/anmpweek01/MainActivity.kt
@@ -2,10 +2,26 @@ package com.viswa.anmpweek01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.viswa.anmpweek01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding
+            .inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this,
+            navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
