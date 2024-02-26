16d72dd7d05eb2c27b03b9b3c205965fd322e193
hidari-nut <madevis02@gmail.com>
2024-02-26 11:49:22 +0700

    Week 2 Exercise Progress added Name Display and Back Button

---
+++ app/src/main/java/com/viswa/anmpweek01/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.viswa.anmpweek01
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.viswa.anmpweek01.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater , container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //val btnStart = view.findViewById<Button>(R.id.btnStart)
+        binding.btnStart.setOnClickListener {
+            val name = binding.txtName.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(name)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+}
